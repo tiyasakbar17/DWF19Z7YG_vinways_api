7c3aab2227dcd2c19d7c34e0efe82d534bb56684
Muhammad Tiyas Fachreza Akbar <tiyas.akbar@gmail.com>
2020-12-01 22:06:34 +0700

    add Artists DB

---
+++ models/artists.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Artists extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Artists.init({
+    name: DataTypes.STRING,
+    old: DataTypes.NUMBER,
+    startCareer: DataTypes.DATE,
+    thumbnail: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Artists',
+  });
+  return Artists;
+};
